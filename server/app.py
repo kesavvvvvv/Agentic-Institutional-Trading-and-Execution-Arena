db8ad85317860e2f704f5a3b16bd2d57bacaf5ca
kesavvvvvv <kesavnimmagadda1@gmail.com>
2026-04-09 01:50:22 +0530

    Made fixes: server/app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
-from src.aitea.api.app import app
+import uvicorn
+
+def main():
+    uvicorn.run(
+        "src.aitea.api.app:app",
+        host="0.0.0.0",
+        port=7860,
+        workers=1
+    )
+
+if __name__ == "__main__":
+    main()
